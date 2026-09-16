8c2b06adc3588faac41aea854829875fe3e8081a
debajoyti007-max <debajoyti007@gmail.com>
2026-09-17 01:26:39 +0530

    feat: replace particle bg with floating transparent real hardware images (motherboard, MacBook logic board, GPU chip)

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import Brands from "./components/Brands/Brands"
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
 import FloatingDock from "./components/FloatingDock/FloatingDock"
-import Scroll3DScene from "./components/Background/Scroll3DScene"
+import HardwareBackground from "./components/Background/HardwareBackground"
 import { LanguageProvider } from "./context/LanguageContext"
 
 export default function App() {
   return (
     <LanguageProvider>
       <div className="min-h-screen bg-[#07070a] text-zinc-100 relative selection:bg-blue-600/30 selection:text-blue-200">
-        {/* Dynamic 3D Scroll & Depth Background Canvas */}
-        <Scroll3DScene />
+        {/* Floating Real Hardware Images Background */}
+        <HardwareBackground />
         <div className="relative z-10">
           <Navbar />
           <main>
